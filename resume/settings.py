59d47581dd7b0112d39d91bf9152a8ef2c3d31f7
ravi9780-hash <56505284+ravi9780-hash@users.noreply.github.com>
2021-06-30 09:52:46 -1200

    m

--- resume/settings.py
+++ resume/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yixqf80)ppon7_!)$f@r!r$gnn&+!ws83^%b_baagm_j!$ifsn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://resumemaker111.herokuapp.com/']
 
 
 # Application definition
