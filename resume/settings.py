0dff9e96af2d9bca8c60505f19f29a50e0c43024
ravi9780-hash <56505284+ravi9780-hash@users.noreply.github.com>
2021-06-30 09:11:22 -1200

    M

--- resume/settings.py
+++ resume/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,4 +122,7 @@ import os
 STATIC_URL = '/static/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
+)
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
